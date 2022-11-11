7837462fb0b87949c0e24aaa7a913edab95bffe8
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2022-11-11 23:44:01 +0900

    Fix the getHours function.
    - Add let 'ten' & 'one' for fix.

--- clock.js
+++ clock.js
@@ -47,53 +47,60 @@ const getDay = function (day) {
   turnOn(1, 4);
 };
 
+// 시간을 가져와서 붙이기
 const getHours = function (hours) {
   // AM or PM
   turnOn(1, 5);
   turnOn(1, 6 + Math.floor(hours / 12));
 
-  // 시간 : 한시 두시 세시 ... 열시 열한시 열두시
+  // "시" 불 키기
   turnOn(3, 7);
-  let hoursPos = hours % 12;
-  // 1 2 3 4 5 6 7 8 9 10 11 0 1 2 3 4 5 6 7 8 9 10 11 0
 
-  if (hoursPos === 1 / 10 >= 1) {
+  let hoursPos = hours % 12;
+  // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+  // 0 1 2 3 4 5 6 7 8 9 10 11 0 1 2 3 4 5 6 7 8 9 10 11
+
+  let ten = hoursPos / 10;
+  let one = hoursPos % 10;
+  // 0 1 2 3 4 5 6 7 8 9 0 1 0 1 2 3 4 5 6 7 8 9 0 1
+
+  // "열" 불키기
+  if (ten >= 1 || ten === 0) {
     turnOn(2, 1);
-  } else if (hoursPos === 0) {
-    turnOn(2, 1);
-    turnOn(2, 3);
   }
 
-  switch (hoursPos) {
+  // "일의 자리" 불키기
+  switch (one) {
     case 1:
     case 2:
     case 3:
     case 4:
-      turnOn(2, 1 + hoursPos);
+      turnOn(2, 1 + one);
       break;
     case 5:
-      turnOn(2, 1 + hoursPos);
+      turnOn(2, 1 + one);
       turnOn(3, 1);
       break;
     case 6:
-      turnOn(3, hoursPos - 6);
-      turnOn(3, hoursPos - 5);
+      turnOn(3, one - 6);
+      turnOn(3, one - 5);
       break;
     case 7:
-      turnOn(3, hoursPos - 5);
-      turnOn(3, hoursPos - 4);
+      turnOn(3, one - 5);
+      turnOn(3, one - 4);
       break;
     case 8:
-      turnOn(3, hoursPos - 8);
-      turnOn(3, hoursPos - 4);
+      turnOn(3, one - 8);
+      turnOn(3, one - 4);
       break;
     case 9:
-      turnOn(3, hoursPos - 4);
-      turnOn(3, hoursPos - 3);
+      turnOn(3, one - 4);
+      turnOn(3, one - 3);
       break;
   }
 };
 
+// 분을 가져와서 불키기
 const getMinutes = function (min) {
   let ten = Math.floor(min / 10);
   let one = min % 10;
@@ -129,6 +136,7 @@ const getMinutes = function (min) {
   turnOn(5, 7);
 };
 
+// 초를 가져와서 불키기
 const getSeconds = function (sec) {
   let ten = Math.floor(sec / 10);
   let one = sec % 10;
@@ -164,15 +172,20 @@ const getSeconds = function (sec) {
   turnOn(7, 7);
 };
 
+// 불키는 함수
 const turnOn = function (row, cloumn) {
   let choiceRow = $("#clock tr")[row];
   let choiceCloumn = $(choiceRow).find("td")[cloumn];
   $(choiceCloumn).addClass("on");
 };
 
+// 불 끄는 함수 (매 초마다 초기화해서 동적으로 보여줌)
 const turnOff = function () {
   $(".on").removeClass("on");
 };
 
+// 웹 구동시 바로 보이도록 함수 지정
 getDate();
+
+// 1초마다 함수 구공하는 함수
 setInterval(getDate, 1000);
